70ef9afce02e1b99ebcf25f51217cd8425c61748
Seah Chen Khoon <chenkhoon@yahoo.com>
2025-04-01 09:54:10 +0800

    docs: Update README.md

--- LLM/02_Generate_unit_test/README.md
+++
@@ -1,7 +0,0 @@
-uv init
-uv add openai
-uv add dotenv
-uv run generate_tests.py
-
-uv run generate_tests.py
-PYTHONPATH=. uv run pytest -v tests/test_ConvertFtToMeters.py
--- LLM/02_Generate_unit_test/src/fizzbuzz.py
+++
@@ -1,9 +0,0 @@
-def fizzbuzz(number):
-    if number % 15 == 0:
-        return "fizz buzz"
-    elif number % 3 == 0:
-        return "fizz"
-    elif number % 5 == 0:
-        return "buzz"
-    else:
-        return number
